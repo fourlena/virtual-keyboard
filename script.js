259dc3a79ecf337e4b412c4d852b883ea268adc0
fourlena <elavlovic@gmail.com>
2022-05-10 00:06:03 +0300

    feat: add init function

--- script.js
+++ script.js
@@ -50,4 +50,38 @@ const Keyboard = {
       english: true,
       curentCursor: 0
     },
+
+    init() {
+        // Create main elements
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
+    
+        // Setup main elements
+        this.elements.main.classList.add("keyboard", "keyboard--hidden");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+        this.elements.keysContainer.appendChild(this._createKeys());
+    
+        this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+    
+        // Add to DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+    
+        // Automatically use keyboard for elements with .use-keyboard-input
+        document.querySelectorAll(".use-keyboard-input").forEach(element => {
+          element.addEventListener("focus", () => {
+            this.open(element.value, currentValue => {
+              element.value = currentValue;
+            });
+          });
+          element.addEventListener("click", () => {
+            element.selectionStart = this.properties.curentCursor;
+            element.selectionEnd = this.properties.curentCursor;
+          });
+        });
+      },
 }
+
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init();
+  }); 
